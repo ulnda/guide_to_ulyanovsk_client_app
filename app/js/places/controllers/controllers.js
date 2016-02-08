e95417b0fc8c5a761915a0093031cfd53613a1e6
d.nagibin <d.nagibin@fun-box.ru>
2016-02-08 14:18:23 +0300

    Place updating.

--- app/js/places/controllers/controllers.js
+++ app/js/places/controllers/controllers.js
@@ -76,8 +76,47 @@ class NewPlaceController {
 }
 NewPlaceController.$inject = ['Place', '$scope', '$state'];
 
+class EditPlaceController {
+  constructor(Place, $scope, $state, $stateParams) {
+    this.place = Place.get({id: $stateParams.id}, () => {
+      this.options = { 
+        map: {
+          center: { 
+            latitude: this.place.latitude, 
+            longitude: this.place.longitude 
+          }, 
+          zoom: 8
+        },
+        point: {
+          id: 0,
+          icon: 'images/marker.png',
+          draggable: true
+        },
+        events: {
+          click: (marker, eventName, args) => {
+            var e = args[0];
+            this.place.latitude = e.latLng.lat();
+            this.place.longitude = e.latLng.lng();
+            $scope.$apply();
+          }
+        }
+      };
+    });
+
+    this.deps = { Place: Place, $state: $state };
+  }
+
+  savePlace() {
+    this.place.$update(() => {
+      this.deps.$state.go('places.all');
+    });
+  }
+}
+EditPlaceController.$inject = ['Place', '$scope', '$state', '$stateParams'];
+
 const controllers = angular.module('app.places.controllers', []);
 
 controllers.controller('PlacesController', PlacesController);
 controllers.controller('NewPlaceController', NewPlaceController);
+controllers.controller('EditPlaceController', EditPlaceController);
 controllers.controller('PlaceRemovingModalController', PlaceRemovingModalController);
